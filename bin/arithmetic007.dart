6f45dea87ad3895e70686fd2bafd03cb1065a3a0
Begzod <ebegzod477@gmail.com>
2022-08-05 17:47:49 +0500

    code007

--- bin/arithmetic007.dart
+++ bin/arithmetic007.dart
@@ -1,4 +1,11 @@
+import 'dart:math';
 // Create function func with arguments
 // the task condition is in the Readme file
 
-void main() {}
+num func(int a, int b) {
+  return a % b;
+}
+
+void main() {
+  print(func(13, 5));
+}
